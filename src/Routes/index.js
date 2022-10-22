1e67e9aef328a21477d56b26c777aeb4ac716e8e
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2022-10-21 22:56:26 -0300

    feat: list hashtags on route GET /hashtags

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import authRouter from "./authRoute.js";
 import postsRouter from "./postsRoute.js";
-import searchRouter from "./searchRoute.js"
+import searchRouter from "./searchRoute.js";
+import hashtagsRouter from "./hashtagsRoute.js";
 
 const router = Router();
 
 router.use(authRouter);
 router.use(postsRouter);
-router.use(searchRouter)
+router.use(searchRouter);
+router.use(hashtagsRouter);
 
 export default router;
