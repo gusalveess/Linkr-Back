d4c0e960fdd27a6837176b3bb02e17e5858ff9f4
Raphael Braga <rbragamouraa@gmail.com>
2022-10-23 13:50:16 -0300

    Repository search add username and picture

--- src/Repositories/searchRepository.js
+++ src/Repositories/searchRepository.js
@@ -3,7 +3,7 @@ import db from "../Database/database.js";
 async function search(id){
 
    return db.query(`
-    SELECT users.username, posts.url,posts.description FROM users
+    SELECT users.username AS from, users.picture AS "userImage", posts.url,posts.description FROM users
     JOIN posts ON users.id = posts."userId"
     WHERE users.id = $1
     `,[id])
