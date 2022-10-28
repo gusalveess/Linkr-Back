cc3212aac98f32e3a309c9f5bac954927f923489
Raphael Braga <rbragamouraa@gmail.com>
2022-10-28 14:27:36 -0300

    searchInput Route

--- src/Routes/searchRoute.js
+++ src/Routes/searchRoute.js
@@ -3,11 +3,13 @@ import { validateSessionByToken } from "../Middlewares/validateTokenMiddleware.j
 import {
 	searchId,
 	listPostsFromUser,
+	searchInput
 } from "../Controllers/searchController.js";
 
 const router = express.Router();
 
 router.get("/user/:id", validateSessionByToken, searchId);
 router.get("/user/:id/posts", validateSessionByToken, listPostsFromUser);
+router.get('/search',validateSessionByToken,searchInput)
 
 export default router;
