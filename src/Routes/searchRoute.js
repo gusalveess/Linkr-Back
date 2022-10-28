95842662834dea62902cac61990524ad79cb3a3a
Ana Fernandes <analuizateixeirafernandes184@gmail.com>
2022-10-28 15:56:44 -0300

    Update searchRoute.js

--- src/Routes/searchRoute.js
+++ src/Routes/searchRoute.js
@@ -2,14 +2,12 @@ import express from "express";
 import { validateSessionByToken } from "../Middlewares/validateTokenMiddleware.js";
 import {
 	searchPeople,
-	listPostsFromUser,
-	searchInput
+	listPostsFromUser
 } from "../Controllers/searchController.js";
 
 const router = express.Router();
 
 router.get("/users", validateSessionByToken, searchPeople);
 router.get("/user/:id/posts", validateSessionByToken, listPostsFromUser);
-router.get('/search',validateSessionByToken,searchInput)
 
 export default router;
